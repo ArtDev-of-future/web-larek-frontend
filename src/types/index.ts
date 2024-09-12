e4c7c5f5d44b62da7c4c59a95db042e0fc52c61c
Artem Voinov <artemvojnov8@gmail.com>
2024-09-12 18:58:37 +0400

    Add: .env file, some information to readme.md and interfaces to index.ts; Fix: index.html, package-lock.jsonl;

---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+interface IItemInit {
+	total: number;
+	items: IItemCard[];
+}
+
+interface IItemCard {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+}
+
+interface ICart {
+	itemCards: IItemCard[];
+	addItemCard(item: IItemCard): void;
+	removeItemCard(id: string): void;
+}
+
+interface ICustomerSet {
+	paymentType: string;
+	address: string;
+	email: string;
+	phone: string;
+	setCustomerInfo(customer: ICustomerSet): void;
+	checkValidation(data: Record<keyof ICustomerSet, string>): boolean;
+}
